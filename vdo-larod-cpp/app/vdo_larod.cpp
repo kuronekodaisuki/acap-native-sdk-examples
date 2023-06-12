2937166a11a292de599f0cff215bd6f733d65649
Shuji Urano <urano_shuji@cyberagent.co.jp>
2023-06-12 14:36:18 +0900

    modify

--- vdo-larod-cpp/app/vdo_larod.cpp
+++ vdo-larod-cpp/app/vdo_larod.cpp
@@ -222,13 +222,21 @@ static void sigintHandler(int sig) {
  * param model Pointer to a larodModel to be obtained.
  * return False if error has occurred, otherwise true.
  */
-static bool setupLarod(const char* chipString, const int larodModelFd,
+static bool setupLarod(const char* chipString, const char* modelFile,
                        larodConnection** larodConn, larodModel** model)
 {
     larodError* error = NULL;
     larodConnection* conn = NULL;
     larodModel* loadedModel = NULL;
     bool ret = false;
+
+    syslog(LOG_INFO, "Create larod models");
+    int larodModelFd = open(modelFile, O_RDONLY);
+    if (larodModelFd < 0) {
+        syslog(LOG_ERR, "Unable to open model file %s: %s", modelFile,
+               strerror(errno));
+        return false;
+    }
 
     // Set up larod connection.
     if (larodConnect(&conn, &error))
@@ -493,6 +501,106 @@ int SetupOverlay()
     setup_axoverlay_data(&data);
 }
 
+bool createAndMapTmpFile(char* fileName, size_t fileSize, void** mappedAddr, int* convFd)
+{
+    syslog(LOG_INFO, "%s: Setting up a temp fd with pattern %s and size %zu", __func__,
+           fileName, fileSize);
+
+    int fd = mkstemp(fileName);
+    if (0 <= fd)
+    {
+      // Allocate enough space in for the fd.
+      if (0 <= ftruncate(fd, (off_t) fileSize))
+      {
+        // Remove since we don't actually care about writing to the file system.
+        if (!unlink(fileName))
+        {
+          // Get an address to fd's memory for this process's memory space.
+          void* data = mmap(NULL, fileSize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+          if (data != MAP_FAILED)
+          {
+            // SUCCESS
+            *mappedAddr = data;
+            *convFd = fd;
+            return true;
+          }
+          else
+          {
+              syslog(LOG_ERR, "%s: Unable to mmap temp file %s: %s", __func__, fileName, strerror(errno));
+          }
+        }
+        else
+        {
+            syslog(LOG_ERR, "%s: Unable to unlink from temp file %s: %s", __func__, fileName, strerror(errno));
+        }
+      }
+      else
+      {
+          syslog(LOG_ERR, "%s: Unable to truncate temp file %s: %s", __func__, fileName, strerror(errno));
+      }
+    }
+    else
+    {
+        syslog(LOG_ERR, "%s: Unable to open temp file %s: %s", __func__, fileName, strerror(errno));
+    }
+
+    // ERROR
+   if (fd >= 0) {
+        close(fd);
+    }
+    return false;
+}
+
+larodMap* CreatePreProcessMap(unsigned int streamWidth, unsigned int streamHeight, unsigned int inputWidth, unsigned int inputHeight)
+{
+  larodError* error = NULL;
+  larodMap* ppMap = larodCreateMap(&error);
+    if (!ppMap) {
+        syslog(LOG_ERR, "Could not create preprocessing larodMap %s", error->msg);
+        return nullptr;
+    }
+    if (!larodMapSetStr(ppMap, "image.input.format", "nv12", &error)) {
+        syslog(LOG_ERR, "Failed setting preprocessing parameters: %s", error->msg);
+        return nullptr;
+    }
+    if (!larodMapSetIntArr2(ppMap, "image.input.size", streamWidth, streamHeight, &error)) {
+        syslog(LOG_ERR, "Failed setting preprocessing parameters: %s", error->msg);
+        return nullptr;
+    }
+    if(chipString != "ambarella-cvflow"){
+        if (!larodMapSetStr(ppMap, "image.output.format", "rgb-interleaved", &error)) {
+            syslog(LOG_ERR, "Failed setting preprocessing parameters: %s", error->msg);
+        return nullptr;
+        }
+    } else {
+        if (!larodMapSetStr(ppMap, "image.output.format", "rgb-planar", &error)) {
+            syslog(LOG_ERR, "Failed setting preprocessing parameters: %s", error->msg);
+        return nullptr;
+        }
+    }
+    if (!larodMapSetIntArr2(ppMap, "image.output.size", inputWidth, inputHeight, &error)) {
+        syslog(LOG_ERR, "Failed setting preprocessing parameters: %s", error->msg);
+        return nullptr;
+    }
+    return ppMap;
+}
+
+larodMap* CreateCropMap(unsigned int clipX, unsigned int clipY, unsigned int clipW, unsigned int clipH)
+{
+  larodError* error = NULL;
+  larodMap* cropMap = larodCreateMap(&error);
+    if (!cropMap) {
+        syslog(LOG_ERR, "Could not create preprocessing crop larodMap %s", error->msg);
+        return nullptr;
+    }
+    if (!larodMapSetIntArr4(cropMap, "image.input.crop", clipX, clipY, clipW, clipH, &error)) {
+        syslog(LOG_ERR, "Failed setting preprocessing parameters: %s", error->msg);
+        return nullptr;
+    }
+    return cropMap;
+}
+
 /**
  * brief Main function that starts a stream with different options.
  */
@@ -593,56 +701,12 @@ int main(int argc, char** argv)
 
     // Create preprocessing maps
     syslog(LOG_INFO, "Create preprocessing maps");
-    ppMap = larodCreateMap(&error);
-    if (!ppMap) {
-        syslog(LOG_ERR, "Could not create preprocessing larodMap %s", error->msg);
-        goto end;
-    }
-    if (!larodMapSetStr(ppMap, "image.input.format", "nv12", &error)) {
-        syslog(LOG_ERR, "Failed setting preprocessing parameters: %s", error->msg);
-        goto end;
-    }
-    if (!larodMapSetIntArr2(ppMap, "image.input.size", streamWidth, streamHeight, &error)) {
-        syslog(LOG_ERR, "Failed setting preprocessing parameters: %s", error->msg);
-        goto end;
-    }
-    if(chipString != "ambarella-cvflow"){
-        if (!larodMapSetStr(ppMap, "image.output.format", "rgb-interleaved", &error)) {
-            syslog(LOG_ERR, "Failed setting preprocessing parameters: %s", error->msg);
-            goto end;
-        }
-    } else {
-        if (!larodMapSetStr(ppMap, "image.output.format", "rgb-planar", &error)) {
-            syslog(LOG_ERR, "Failed setting preprocessing parameters: %s", error->msg);
-            goto end;
-        }
-    }
-    if (!larodMapSetIntArr2(ppMap, "image.output.size", inputWidth, inputHeight, &error)) {
-        syslog(LOG_ERR, "Failed setting preprocessing parameters: %s", error->msg);
-        goto end;
-    }
-    cropMap = larodCreateMap(&error);
-    if (!cropMap) {
-        syslog(LOG_ERR, "Could not create preprocessing crop larodMap %s", error->msg);
-        goto end;
-    }
-    if (!larodMapSetIntArr4(cropMap, "image.input.crop", clipX, clipY, clipW, clipH, &error)) {
-        syslog(LOG_ERR, "Failed setting preprocessing parameters: %s", error->msg);
-        goto end;
-    }
+    ppMap = CreatePreProcessMap(streamWidth, streamHeight, inputWidth, inputHeight);
+    cropMap = CreateCropMap(clipX, clipY, clipW, clipH);
 
     // Create larod models
-    syslog(LOG_INFO, "Create larod models");
-    larodModelFd = open(modelFile, O_RDONLY);
-    if (larodModelFd < 0) {
-        syslog(LOG_ERR, "Unable to open model file %s: %s", modelFile,
-               strerror(errno));
-        goto end;
-    }
-
-
     syslog(LOG_INFO, "Setting up larod connection with chip %s and model file %s", chipString, modelFile);
-    if (!setupLarod(chipString, larodModelFd, &conn, &model)) {
+    if (!setupLarod(chipString, modelFile, &conn, &model)) {
         goto end;
     }
 
@@ -785,6 +849,7 @@ int main(int argc, char** argv)
         goto end;
     }
 
+    // LOOP
     for (unsigned int i = 0; i < numRounds && !stopRunning; i++)
     {
         struct timeval startTs, endTs;
@@ -793,7 +858,7 @@ int main(int argc, char** argv)
         // Get latest frame from image pipeline.
         VdoBuffer* buf = getLastFrameBlocking(provider);
         if (!buf) {
-            goto end;
+            break;
         }
 
         // Get data from latest frame.
@@ -802,10 +867,11 @@ int main(int argc, char** argv)
         // Covert image data from NV12 format to interleaved uint8_t RGB format
         gettimeofday(&startTs, NULL);
         memcpy(ppInputAddr, nv12Data, yuyvBufferSize);
-        if (!larodRunJob(conn, ppReq, &error)) {
+        if (!larodRunJob(conn, ppReq, &error))
+        {
             syslog(LOG_ERR, "Unable to run job to preprocess model: %s (%d)",
                    error->msg, error->code);
-            goto end;
+            break;
         }
         gettimeofday(&endTs, NULL);
 
@@ -822,13 +888,13 @@ int main(int argc, char** argv)
         if (lseek(larodOutput1Fd, 0, SEEK_SET) == -1) {
             syslog(LOG_ERR, "Unable to rewind output file position: %s",
                    strerror(errno));
-            goto end;
+            break;
         }
 
         if (lseek(larodOutput2Fd, 0, SEEK_SET) == -1) {
             syslog(LOG_ERR, "Unable to rewind output file position: %s",
                    strerror(errno));
-            goto end;
+            break;
         }
 
         gettimeofday(&startTs, NULL);
@@ -838,7 +904,7 @@ int main(int argc, char** argv)
         {
             syslog(LOG_ERR, "Unable to run inference on model %s: %s (%d)",
                    modelFile, error->msg, error->code);
-            goto end;
+            break;
         }
         gettimeofday(&endTs, NULL);
 
@@ -869,11 +935,10 @@ int main(int argc, char** argv)
     }
 
     syslog(LOG_INFO, "Stop streaming video from VDO");
-    if (!stopFrameFetch(provider)) {
-        goto end;
+    if (stopFrameFetch(provider))
+    {
+      ret = true;
     }
-
-    ret = true;
 
 end:
     if (provider) {
