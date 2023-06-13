68f12d5e7d759a9feb47da26239f6362d3ddfd9b
Hiro Matsuoka <kuronekodaisuki@me.com>
2023-06-13 21:25:15 +0900

    クラスLabels

---
+++ snow-depth/app/Labels.cpp
@@ -0,0 +1,159 @@
+//
+//
+//
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include "Labels.h"
+
+
+const char*Labels::operator[](unsigned int index)
+{
+    if (index < _labels.size())
+        return _labels[index].c_str();
+    else 
+        return nullptr;
+}
+
+/**
+ * @brief Reads a file of labels into an array.
+ *
+ * An array filled by this function should be freed using freeLabels.
+ *
+ * @param labelsPtr Pointer to a string array.
+ * @param labelFileBuffer Pointer to the labels file contents.
+ * @param labelsPath String containing the path to the labels file to be read.
+ * @param numLabelsPtr Pointer to number which will store number of labels read.
+ * @return False if any errors occur, otherwise true.
+ */
+size_t Labels::Load(const char* filename) 
+{
+    // We cut off every row at 60 characters.
+    const size_t LINE_MAX_LEN = 60;
+    bool ret = false;
+    char* labelsData = NULL;  // Buffer containing the label file contents.
+    //char** labelArray = NULL; // Pointers to each line in the labels text.
+
+    struct stat fileStats = { 0 };
+    if (stat(filename, &fileStats) < 0) {
+        syslog(LOG_ERR, "%s: Unable to get stats for label file %s: %s", __func__,
+            labelsPath, strerror(errno));
+        return false;
+    }
+
+    // Sanity checking on the file size - we use size_t to keep track of file
+    // size and to iterate over the contents. off_t is signed and 32-bit or
+    // 64-bit depending on architecture. We just check toward 10 MByte as we
+    // will not encounter larger label files and both off_t and size_t should be
+    // able to represent 10 megabytes on both 32-bit and 64-bit systems.
+    if (fileStats.st_size > (10 * 1024 * 1024)) {
+        syslog(LOG_ERR, "%s: failed sanity check on labels file size", __func__);
+        return false;
+    }
+
+    size_t labelsFileSize = (size_t)fileStats.st_size;
+    // Allocate room for a terminating NULL char after the last line.
+    labelsData = new char[labelsFileSize + 1];
+    if (labelsData == NULL) {
+        syslog(LOG_ERR, "%s: Failed allocating labels text buffer: %s", __func__,
+            strerror(errno));
+        return 0;
+    }
+
+    int labelsFd = open(filename, O_RDONLY);
+    if (labelsFd < 0) {
+        syslog(LOG_ERR, "%s: Could not open labels file %s: %s", __func__, labelsPath,
+            strerror(errno));
+        return 0;
+    }
+
+    /*
+    size_t numBytesRead = -1;
+    size_t totalBytesRead = 0;
+    char* fileReadPtr = labelsData;
+    while (totalBytesRead < labelsFileSize) 
+    {
+        numBytesRead = read(labelsFd, fileReadPtr, labelsFileSize - totalBytesRead);
+
+        if (numBytesRead < 1) {
+            syslog(LOG_ERR, "%s: Failed reading from labels file: %s", __func__,
+                strerror(errno));
+            goto end;
+        }
+        totalBytesRead += (size_t)numBytesRead;
+        fileReadPtr += numBytesRead;
+    }
+    */
+    read(labelsFd, labelsData, labelsFileSize);
+    close(labelsFd);
+
+    /*
+    // Now count number of lines in the file - check all bytes except the last
+    // one in the file.
+    size_t numLines = 0;
+    for (size_t i = 0; i < (labelsFileSize - 1); i++) 
+    {
+        if (labelsData[i] == '\n') {
+            numLines++;
+        }
+    }
+
+    // We assume that there is always a line at the end of the file, possibly
+    // terminated by newline char. Either way add this line as well to the
+    // counter.
+    numLines++;
+    */
+
+    //size_t labelIdx = 0;
+    //labelArray[labelIdx] = labelsData;
+    //labelIdx++;
+    size_t delimiter = 0;
+    for (size_t i = 0; i < labelsFileSize; i++) 
+    {
+        if (labelsData[i] == '\n') 
+        {
+            std::string label(&labelsData[delimiter], i - delimiter);
+            _labels.push_back(label);
+            delimiter = i + 1;
+            /*
+            // Register the string start in the list of labels.
+            labelArray[labelIdx] = labelsData + i + 1;
+            labelIdx++;
+            // Replace the newline char with string-ending NULL char.
+            labelsData[i] = '\0';
+            */
+        }
+    }
+
+    /*
+    // If the very last byte in the labels file was a new-line we just
+    // replace that with a NULL-char. Refer previous for loop skipping looking
+    // for new-line at the end of file.
+    if (labelsData[labelsFileSize - 1] == '\n') {
+        labelsData[labelsFileSize - 1] = '\0';
+    }
+
+    // Make sure we always have a terminating NULL char after the label file
+    // contents.
+    labelsData[labelsFileSize] = '\0';
+
+    // Now go through the list of strings and cap if strings too long.
+    for (size_t i = 0; i < numLines; i++) 
+    {
+        size_t stringLen = strnlen(labelArray[i], LINE_MAX_LEN);
+        if (stringLen >= LINE_MAX_LEN) {
+            // Just insert capping NULL terminator to limit the string len.
+            *(labelArray[i] + LINE_MAX_LEN + 1) = '\0';
+        }
+    }
+
+    *labelsPtr = labelArray;
+    *numLabelsPtr = numLines;
+    *labelFileBuffer = labelsData;
+    */
+
+    delete[] labelsData;
+    return _labels.size();
+}
---
+++ snow-depth/app/Labels.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+#ifdef WIN32
+#ifdef EXPORT
+#define API __declspec(dllexport)
+#else
+#define API __declspec(dllimport)
+#endif
+#include <io.h>
+#define syslog()
+#pragma warning(disable: 4996)
+#else
+#define API
+#include <unistd.h>
+#include <syslog.h>
+#endif
+
+class API Labels
+{
+public:
+	//Labels();
+	//~Labels();
+
+	size_t Load(const char* filename);
+	size_t GetCount() { return _labels.size(); }
+	const char* operator[](unsigned int index);
+
+private:
+	std::vector<std::string> _labels;
+};
+
