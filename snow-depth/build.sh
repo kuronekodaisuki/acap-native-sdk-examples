964252f84c289ec2b8151cac0c038c6dbc2cb84e
Shuji Urano <urano_shuji@cyberagent.co.jp>
2023-06-13 21:29:12 +0900

    Modify build.sh

--- snow-depth/build.sh
+++ snow-depth/build.sh
@@ -4,6 +4,6 @@ export ARCH=armv7hf
 export DOCKER_BUILDKIT=1
 export APP_NAME=snow-depth:1.0
 
-docker build --tag ${APP_NAME} .
+docker build --build-arg CHIP=cpu --tag ${APP_NAME} .
 
 docker cp $(docker create ${APP_NAME}):/opt/app ./build
