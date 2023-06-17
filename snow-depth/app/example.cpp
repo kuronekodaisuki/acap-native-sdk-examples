0a981a5ea0464cf567a088e9f07d372220cc5556
Shuji Urano <urano_shuji@cyberagent.co.jp>
2023-06-17 14:19:35 +0900

    Change model to yolox_nano_int8_quantize.tflite

--- snow-depth/app/example.cpp
+++ snow-depth/app/example.cpp
@@ -90,7 +90,7 @@ int main(int argc, char* argv[])
   Larod larod(streamWidth, streamHeight);
   larod.EnumerateDevices();
 
-  larod.LoadModel("model/converted_model.tflite", 300, 300);
+  larod.LoadModel("model/yolox_nano_int8_quantize.tflite", 416, 416);
   larod.LoadLabels("label/labels.txt");
 
   provider = createImgProvider(streamWidth, streamHeight, 2, VDO_FORMAT_YUV);
